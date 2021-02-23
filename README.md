c523280a56d33a806d20339a59f26f9559180e8f
Filip Korus <filipkorus123@gmail.com>
2021-02-23 20:48:26 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# drag-and-drop-files-uploading
+
+### About
+This project is an example of drag & drop files uploading. It also has a progressbar.
+	
+## Technologies
+Project is created with:
+* PHP
+* JavaScript
+* Bootstrap 5
+
+![droparea](https://github.com/filipkorus/drag-and-drop-files-uploading/blob/uploads/1.png)
+![dragged](https://github.com/filipkorus/drag-and-drop-files-uploading/blob/uploads/2.png)
+![uploading](https://github.com/filipkorus/drag-and-drop-files-uploading/blob/uploads/3.png)
+![uploaded](https://github.com/filipkorus/drag-and-drop-files-uploading/blob/uploads/4.png)
---
+++ upload.php
@@ -0,0 +1,36 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === "POST") {
+
+   header('Access-Cotrol-Allow-Origin: *');
+   header('Content-Type: application/json');
+
+   if (isset($_FILES["myFiles"])) {
+
+      $uploaded = array();
+
+      foreach ($_FILES['myFiles']['tmp_name'] as $key => $value) {
+
+         $upload_folder = './uploads/';
+
+         $target_dir = $upload_folder . basename($_FILES['myFiles']['name'][$key]);
+
+         $file_count = 1;
+         $new_dir = $target_dir;
+         while (file_exists($new_dir)) {
+            $new_dir = $target_dir;
+            $new_dir = substr($new_dir, 0, strrpos($new_dir, ".")) . "($file_count)" . substr($new_dir, strrpos($new_dir, "."));
+            $file_count++;
+         }
+         
+         $success = move_uploaded_file($value, $new_dir);
+
+         array_push($uploaded, array(
+            'success' => $success,
+            'file_name' => $_FILES['myFiles']['name'][$key],
+            'new_file_name' => str_replace('./uploads/', '', $new_dir)
+         ));
+      }
+
+      echo json_encode($uploaded);
+   }
+}
